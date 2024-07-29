5ac259d5b5dc3a90bf4cf6c803059fd9aa59215e
Zhampy <62169874+SuhaibChoudhary@users.noreply.github.com>
2024-07-29 16:39:51 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Serve static files like CSS
+app.use(express.static('public'));
+
+// Serve the homepage
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
